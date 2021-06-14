06f68ebd872a77299d1bb2c517513d3e15069360
Sraddha <8770473516sraddha@gmail.com>
2021-06-14 14:58:55 +0530

    Voting System

---
+++ Blockchain-Election-main/truffle-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
+  },
+   compilers: {
+    solc: {
+      version: "^0.4.20", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+     
+      
+    }
+  }
+};
